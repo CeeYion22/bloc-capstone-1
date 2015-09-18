bc90619282b63b2379481c747fd32b0d0fa3dbb9
UnicycleMatt <matt@unicyclelabs.com>
2015-09-17 23:49:58 -0700

    Basic web service in place for restaurants

--- server/server.py
+++ server/server.py
@@ -1,21 +1,95 @@
 import flask
+from flask import abort
 from flask import json
-from flask import render_template
+from flask import make_response
 from flask import request
 
 from app import app
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    template_params = {
-        'foo': 'bar',
-        'blah': 'blah'
-    }
-    # ** is used to convert the dictionary into key value pairs
-    # return render_template('index.html', **template_params)
+# Temporary test data - remove this later
 
-    # return render_template('manage/index.html', foo=bar, blah=blah)
-    return json.jsonify({'foo': 'bar'})
+restaurants = [
+    {
+        'id': 1,
+        'name': 'Burger King',
+        'description': 'Whoppers and shit'
+    },
+    {
+        'id': 2,
+        'name': 'McDonalds',
+        'description': 'Big Mac Shit'
+    }
+]
+
+menu_items = [
+    {
+        'id': 1,
+        'restaurant_id': 1,
+        'name': 'Secret shit',
+        'description': 'Lots of tomatoes'
+    },
+    {
+        'id': 2,
+        'restaurant_id': 1,
+        'name': 'Loads of crap',
+        'description': 'Super Yummy'
+    }
+]
+
+@app.route('/api/v1.0/restaurants', methods=['GET'])
+def get_restaurants():
+    return json.jsonify({'restaurants': restaurants})
+
+# Need to add authentication for admin account
+@app.route('/api/v1.0/restaurants', methods=['POST'])
+def create_restaurant():
+    if not request.json or not 'name' in request.json:
+        abort(404)
+    
+    # This is just a temporary hack. Need to create a service call to controller that inserts to DB
+    restaurant = {
+        'id': restaurants[-1]['id'] + 1,
+        'name': request.json['name'],
+        'description': request.json.get('description', "")
+    }
+    restaurants.append(restaurant)
+    print restaurants
+ 
+    return json.jsonify({'restaurant': restaurant}), 201
+
+@app.route('/api/v1.0/restaurants/<int:restaurant_id>', methods=['GET'])
+def get_restaurant(restaurant_id):
+    restaurant = [restaurant for restaurant in restaurants if restaurant['id'] == restaurant_id]
+    if len(restaurant) == 0:
+        abort(404)
+    return json.jsonify({'restaurant': restaurant[0]})
+
+@app.route('/api/v1.0/restaurants/<int:restaurant_id>', methods=['PUT'])
+def update_restaurant(restaurant_id):
+    restaurant = [restaurant for restaurant in restaurants if restaurant['id'] == restaurant_id]
+    if len(restaurant) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'name' in request.json and type(request.json['name']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    restaurant[0]['name'] = request.json.get('name', restaurant[0]['name'])
+    restaurant[0]['description'] = request.json.get('description', restaurant[0]['description'])
+    return json.jsonify({'restaurant': restaurant[0]})
+
+@app.route('/api/v1.0/restaurants/<int:restaurant_id>', methods=['DELETE'])
+def delete_restaurant(restaurant_id):
+    restaurant = [restaurant for restaurant in restaurants if restaurant['id'] == restaurant_id]
+    if len(restaurant) == 0:
+        abort(404)
+    restaurants.remove(restaurant[0])
+    return json.jsonify({'result': True})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(json.jsonify({'error': 'Not found'}), 404)
 
 # This is just for debug mode so you don't have to run Apache
 if __name__ == '__main__':
